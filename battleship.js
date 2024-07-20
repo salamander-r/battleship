1d758ac15299577e0833e8809cb9937657bb6231
salamander-r <pyvovar.roman9754@gmail.com>
2024-07-20 13:00:30 +0300

    added new changes

--- battleship.js
+++ battleship.js
@@ -30,12 +30,17 @@ var view = {
         var index = ship.locations.indexOf(guess);
         if (index >= 0) {
           ship.hits[index] = "hit";
+          view.displayHit(guess);
+          view.displayMessage("HIT!");
           if (this.isSunk(ship)) {
+            view.displayMessage("You sank my battleship!");
             this.shipsSunk++;
           }
           return true;
         }
       }
+      view.displayMiss(guess);
+      view.displayMessage("You missed.");
       return false;
     },
     isSunk: function(ship) {
