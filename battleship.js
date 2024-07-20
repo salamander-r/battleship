954941717852782e9fefb8fb19ae3a39889e99be
salamander-r <pyvovar.roman9754@gmail.com>
2024-07-20 10:37:05 +0300

    added model (new changes)

--- battleship.js
+++ battleship.js
@@ -14,11 +14,19 @@ var view = {
       cell.setAttribute("class", "miss");
     }
   }
-  view.displayMiss("00");
-  view.displayHit("34");
-  view.displayMiss("55");
-  view.displayHit("12");
-  view.displayMiss("25");
-  view.displayHit("26");
 
-  view.displayMessage("Tap tap, is this thing on?");
+  var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipsSunk: 0,
+    shipLength: 3,
+    ships: [ {locations: ["06", "16", "26"], hits: ["", "", ""] },
+             {locations: ["24", "34", "44"], hits: ["", "", ""] },
+             {locations: ["10", "11", "12"], hits: ["", "", ""] }],
+    
+    fire: function(guess) {
+      for (var i = 0; i < this.numShips; i++) {
+        var ship = this.ships[i];
+      }
+    }
+  }
